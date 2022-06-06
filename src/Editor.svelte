946636eb6afdeefe6a5b5df13a2cdb41b84a7f27
Matt Nichols <mattnichols206@gmail.com>
2022-06-05 17:12:37 -0700

    Rework editor for improved behavior

--- src/Editor.svelte
+++ src/Editor.svelte
@@ -22,52 +22,99 @@
         });
     }
 
-    function updateEditor(forceUpdate) {
+    function updateEditor(forceGeneration, forceDisplayUpdate) {
+        console.log(window.getSelection());
         const caretPosition = caretHandler.getCurrentCaretPosition();
         let textBeforeCursor = editorDiv.innerText.substring(0, caretPosition);
-        let triggerGeneration = caretPosition > lastCaretPosition || (forceUpdate === true);
+        let triggerGeneration = caretPosition > lastCaretPosition || (forceGeneration === true);
+        
+        if (!triggerGeneration || forceDisplayUpdate === true) {
+            // Otherwise, make sure style is updated
+            const primaryText = editorDiv.innerText.substring(0, caretPosition);
+            const secondaryText = triggerGeneration ? "" : editorDiv.innerText.substring(caretPosition, editorDiv.innerText.length);
+            displayText(primaryText, secondaryText, caretPosition);
+        } 
     
-        // Display update editor prior to generation
-        const primaryText = editorDiv.innerText.substring(0, caretPosition);
-        const secondaryText = triggerGeneration ? "" : editorDiv.innerText.substring(caretPosition, editorDiv.innerText.length);
-        displayText(primaryText, secondaryText, caretPosition);
-
-        // Generate if need be, which will trigger another editor update
         if (triggerGeneration) {
+            // Generate if need be, which will trigger another editor update
             generate(textBeforeCursor);
-        }
+            console.log("generating");
+        } 
     }
 
     function displayText(primaryText, secondaryText, caretPosition) {
         if (!caretPosition) {
             caretPosition = caretHandler.getCurrentCaretPosition();
         }
-        const primaryTextHTML = primaryText ? "<span class='primaryText'>" + primaryText + "</span>" : "";
-        const secondaryTextHTML = secondaryText ? "<span class='secondaryText'>" + secondaryText + "</span>" : "";
+        const primaryTextHTML = primaryText ? "<span id='primaryText'>" + primaryText + "</span>" : "";
+        const secondaryTextHTML = secondaryText ? "<span id='secondaryText'>" + secondaryText + "</span>" : "";
         editorDiv.innerHTML = primaryTextHTML + secondaryTextHTML;
         caretHandler.setCurrentCaretPosition(caretPosition);
         lastCaretPosition = caretPosition;
     }
 
-    function updateDelayed(event) {
-        // TODO - only delay the stuff that ACTUALLY needs to be delayed, and do the rest
-        // immediately / before rendering. This at least must include clearing the previous
-        // suggestion, if applicable (so it doesn't scoot with the cursor)
+    function delayed(fn) {
+        // Some things need to happen after text input is resolved
+        setTimeout(fn, 100);
+    }
 
-        // Update delayed for keydown, thus taking into account new caret position.
-        setTimeout(() => {
-            // Force update w/ backspace
-            updateEditor(event.key === "Backspace");
-        }, 1);
+    function clearSecondaryText() {
+        const secondaryTextEl = document.getElementById("secondaryText");
+        if (secondaryTextEl) secondaryTextEl.remove();
+    }
+
+    function onKeyDown(event) {
+        // Don't allow enter keys
+        if (event.key === "Enter") {
+            event.preventDefault();
+            return;
+        }
+
+        // Don't allow changing selected text (too hard)
+        const selection = window.getSelection();
+        if (selection.anchorOffset != selection.focusOffset) {
+            event.preventDefault();
+            return;
+        }
+
+        // Ignore modified inputs
+        if (event.metaKey) {
+            return;
+        }
+
+        // Ignore non-input key presses
+        if (event.keyCode == "9" || event.keyCode =="16" || event.keyCode =="17" || event.keyCode =="18" || event.keyCode == "91") {
+            return;
+        }
+
+        // Handle different types of allowed input
+        if (event.keyCode == "37" || event.keyCode == "38" || event.keyCode == "39" || event.keyCode == "40") { // Arrow keys
+            // Wait for cursor to move, then update the display
+            delayed(() => { updateEditor(false, true); });
+        } else if (event.keyCode == "8" || event.keyCode == "46") { // Backspace or Delete
+            // Clear then generate new text
+            clearSecondaryText();
+            delayed(() => { updateEditor(true, false); });
+        } else { // Text input (or I've forgotten something)
+            // Clear, then update
+            clearSecondaryText();
+            delayed(updateEditor);
+        }
+    }
+
+    function onClick(event) {
+        const selection = window.getSelection();
+        if (selection.anchorOffset == selection.focusOffset) {
+            updateEditor(false, true); // no need to delay!
+        }
     }
 </script>
 
 <div
     contenteditable="true"
     bind:this={editorDiv}
-    on:keydown={updateDelayed}
-    on:click={updateEditor}
-    on:keypress={(event) => { if (event.key === "Enter") event.preventDefault(); }}
+    on:click={onClick}
+    on:keydown={onKeyDown}
     on:paste={(event) => { event.preventDefault(); }}
 />
 
@@ -88,11 +135,11 @@
         font-size: 24px;
     }
 
-    :global(.primaryText) {
+    :global(#primaryText) {
         color: #000;
     }
 
-    :global(.secondaryText) {
+    :global(#secondaryText) {
         color: #bbb;
     }
 </style>
